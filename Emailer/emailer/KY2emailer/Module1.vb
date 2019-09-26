929fe433960ad7d7403f1f053dc656fed0eb9fae
jason-sanchez <jsanchez@systemaxcorp.com>
2019-09-26 13:15:35 -0400

    New Changes

---
+++ Emailer/emailer/KY2emailer/Module1.vb
@@ -0,0 +1,127 @@
+﻿'20170111 - Change relay to relay.catholichealth.net port 25 per Mohammed 12/30/2016
+
+Imports System.Net.Mail
+Imports System.IO
+Imports System.Configuration
+
+
+Module Module1
+
+    Dim fileCount As Integer = 0
+    Dim strMessage As String = ""
+    Dim globalError As Boolean = False
+    Dim gblLogString As String = ""
+    Dim DFTErrorDir As DirectoryInfo = New DirectoryInfo(System.Configuration.ConfigurationManager.AppSettings("DFTErrorDir"))
+    Dim strLogDirectory As String = System.Configuration.ConfigurationManager.AppSettings("ErrorLog").ToString()
+
+    Sub Main()
+
+        Try
+            Call process_SpaldingDFT()
+
+
+            If fileCount > 0 Then
+                mailIT(strMessage)
+            End If
+
+        Catch ex As Exception
+            globalError = True
+
+            gblLogString = gblLogString & "Spalding Emailer Error" & vbCrLf
+            gblLogString = gblLogString & ex.Message & vbCrLf
+            gblLogString = gblLogString & strMessage & vbCrLf
+            writeToLog(gblLogString, 1)
+
+            Exit Sub
+        End Try
+
+    End Sub
+
+    Private Sub process_SpaldingDFT()
+        Try
+
+
+            Dim DFTErrorDirStr As String = System.Configuration.ConfigurationManager.AppSettings("DFTErrorDir").ToString()
+            strMessage = strMessage & vbCrLf
+
+            strMessage = strMessage & "Spalding DFT Status as of " & CStr(DateTime.Now) & vbCrLf
+
+            Dim DFTerrorFiles As FileInfo() = DFTErrorDir.GetFiles("*.*")
+
+
+            If DFTerrorFiles.Length = 1 Then
+                strMessage = strMessage & "There is " & CStr(DFTerrorFiles.Length) & " file in the backup directory. The Error File is located at... " & DFTErrorDirStr & " & vbCrLf"
+            Else
+                strMessage = strMessage & "There are " & CStr(DFTerrorFiles.Length) & " files in the backup directory. The Error Files is located at... " & DFTErrorDirStr & " & vbCrLf"
+            End If
+
+            fileCount = fileCount + DFTerrorFiles.Length
+
+        Catch ex As Exception
+
+            gblLogString = gblLogString & "process Spalding DFT Error" & vbCrLf
+            gblLogString = gblLogString & ex.Message & vbCrLf
+
+            Exit Sub
+        End Try
+    End Sub
+ 
+
+    Private Sub writeToLog(ByVal strMsg As String, ByVal eventType As Integer)
+
+        Dim file As System.IO.StreamWriter
+        Dim tempLogFileName As String = strLogDirectory & "SpaldingEmailer_log.txt"
+        file = My.Computer.FileSystem.OpenTextFileWriter(tempLogFileName, True)
+        file.WriteLine(DateTime.Now & " : " & strMsg)
+        file.Close()
+
+    End Sub
+
+
+    Private Sub mailIT(strMessage As String)
+        Dim SMTP As New SmtpClient(System.Configuration.ConfigurationManager.AppSettings("email_server").ToString())
+
+        'Dim strServer As String = objIniFile.GetString("Email Settings", "email_server", "(none)")
+        Dim strFromAddress As String = System.Configuration.ConfigurationManager.AppSettings("email_from").ToString()
+        Dim strToAddress As String = System.Configuration.ConfigurationManager.AppSettings("email_to").ToString()
+
+        Dim message As New MailMessage(strFromAddress, strToAddress)
+        Dim username As String = System.Configuration.ConfigurationManager.AppSettings("username").ToString()
+        Dim password As String = System.Configuration.ConfigurationManager.AppSettings("password").ToString()
+        Dim port As Integer = CInt(System.Configuration.ConfigurationManager.AppSettings("port"))
+
+
+        '20150629 - changed smtpclient back to relay.jhsmh.root.local
+        ' emailSender = New System.Net.Mail.SmtpClient("10.48.9.181")
+        'emailSender = New System.Net.Mail.SmtpClient("relay.jhsmh.root.local")
+        '20170111 - Change relay to relay.catholichealth.net port 25 per Mohammed 12/30/2016
+        'emailSender = New System.Net.Mail.SmtpClient("relay.catholichealth.net")
+        'emailSender.Port = 25
+        'emailSender.Port = port
+
+        ' ----- Build the content details.
+        Try
+
+            SMTP.EnableSsl = True
+            SMTP.Credentials = New Net.NetworkCredential(username, password)
+            SMTP.Port = port
+
+            message.Subject = "Spalding Production Emailer Message " & CStr(Now)
+            message.Body = strMessage
+
+            SMTP.Send(message)
+
+        Catch ex As Exception
+            globalError = True
+
+            gblLogString = gblLogString & "MailIt Error" & vbCrLf
+            gblLogString = gblLogString & ex.Message & vbCrLf
+            writeToLog(gblLogString, 1)
+            Exit Sub
+        Finally
+        End Try
+    End Sub
+
+
+
+End Module
---
+++ Emailer/emailer/KY2emailer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("KY2emailer")> 
+<Assembly: AssemblyDescription("production.20170111 - Change relay to relay.catholichealth.net port 25 per Mohammed 12/30/2016")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("KY2emailer")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c378a30d-bac6-4760-a309-93611c787b7a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
